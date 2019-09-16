2753713951a8ad7fcd045904bc951e7a275b7f9d
morry48 <yyyyuuuuuuuu0408@gmail.com>
2019-09-16 21:05:50 +0900

    a
    A

---
+++ app/Tweet.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\softDeletes;
+
+class Tweet extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'text'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getTimeLines()
+    {
+        return $this->with('user')->orderBy('tweets.created_at', 'DESC')->paginate(5);
+    }
+}
